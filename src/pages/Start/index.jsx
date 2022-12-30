ec0dcae2610d5e2ac5f62b685823b21fdc4ac731
MAugusto89 <souzadagama966.masdg@gmail.com>
2022-12-30 10:18:29 -0400

    Estilização da página Start

--- src/pages/Start/index.jsx
+++ src/pages/Start/index.jsx
@@ -6,15 +6,35 @@ export default function Start() {
     const navigation = useNavigation();
 
     return (
-        <View>
+        <View style={styles.container}>
             <ScrollView showsVerticalScrollIndicator={false}>
-                <View>
+                <View style={{alignItems:"center"}}>
                     <Image
                         source={require("../../assets/icons/logo3.png")} 
+                        style={styles.logo}
                     />
-                    <Text>Vamos transformar sua vida em um jogo</Text>
+                    <Text style={styles.description}>Vamos transformar sua vida em um jogo</Text>
                 </View>
             </ScrollView>
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    container:{
+        flex: 1,
+        backgroundColor: "rgba(21, 21, 21, 0.98)",
+    },
+    logo: {
+        width: 300,
+        height: 60,
+        marginTop: 60,
+        marginBottom:20,
+    },
+    description: {
+        color:"#ffffff",
+        fontSize: 20,
+        textAlign: "center",
+        marginVertical: 60,
+    }
+})
